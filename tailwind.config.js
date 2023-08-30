26a5ceb4e09a2425d790bc97b7f64ef977faf57f
mamun <mamunbhuiyan919@gmail.com>
2023-08-30 22:46:45 +0600

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+				'xs':'480px'
+			},
+      boxShadow: {
+        'primary': '0px 0px 5px rgba(200, 201, 209, 0.65)',
+      },
+
+      borderColor: {
+        'primary': 'rgba(113, 106, 133, 0.25)'
+      },
+      divideColor: {
+        'primary': 'rgba(113, 106, 133, 0.25)'
+      },
+      colors:{
+        'primary': {
+          light: '#67e8f9',
+          DEFAULT: '#06b6d4',
+          dark: '#0e7490',
+        },
+      }
+    }
   },
   plugins: [],
 }
